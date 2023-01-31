b886873606d9f61462849134ff7595d49fa27e6d
VienVu <vienvt@bitelle.com?>
2023-01-31 14:19:17 +0700

    aupdare native

--- lib/screen/list.dart
+++ lib/screen/list.dart
@@ -10,22 +10,18 @@ class ListPage extends StatelessWidget {
       body: const Center(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.black,
         onTap: (index) {},
+        currentIndex: 0,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(
               Icons.account_tree_outlined,
-              color: Colors.black,
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.abc_rounded,
               color: Colors.black,
             ),
             label: 'Home',
